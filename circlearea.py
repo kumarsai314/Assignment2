00d1d0c171fb82d7fcb41e377d0bc6c885dcfe93
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-04-02 16:21:53 -0700

    Area & perimeter of a circle

---
+++ circlearea.py
@@ -0,0 +1,7 @@
+def area(r):
+    return 3.14*r*r
+def perimeter(r):
+    return 2*3.14*r
+r=int(input('Enter the radius:'))
+print('Area of circle :',area(r))
+print('perimeter of circle :',perimeter(r))
